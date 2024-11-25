1f2e52d669e6b9e8ff5c18b1502b4575e1023a2a
Noraway <113176708+Noraway@users.noreply.github.com>
2024-11-25 19:54:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4th program
+string = '123.456'
+string = float(string)
+first_digit = int(string * 10 % 10)
+print(first_digit)
